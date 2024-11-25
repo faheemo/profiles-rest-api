9ecdbb1bc0757bfdbe7f7e389aa13c62797d2c23
Faheem Moolla <faheemmoolla1@gmail.com>
2024-11-25 16:01:49 +0200

    Added search profiles ability

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status, viewsets
 from profiles_api import serializers, models, permission
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 
 
@@ -97,3 +98,5 @@ class UserProfileViewSet (viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permission.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
